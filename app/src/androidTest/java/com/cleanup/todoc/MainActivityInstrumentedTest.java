1db8bd41285a0f144eb4cfe4e245c4fe60e9a065
Azubal <valentinfamery087@gmail.com>
2021-06-04 17:19:55 +0200

    Commit 2.4 Add Report for all tests

--- app/src/androidTest/java/com/cleanup/todoc/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/cleanup/todoc/MainActivityInstrumentedTest.java
@@ -1,31 +1,20 @@
 package com.cleanup.todoc;
 
-
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
-
-import com.cleanup.todoc.model.Project;
 import com.cleanup.todoc.ui.MainActivity;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.cleanup.todoc.TestUtils.withRecyclerView;
