7432c808329c8fc82afc855f4887545d7188d5f6
Azubal <valentinfamery087@gmail.com>
2021-06-10 00:28:15 +0200

    Commit 3.0 Clean Code

--- app/src/main/java/com/azubal/todoc/ui/TasksAdapter.java
+++ app/src/main/java/com/azubal/todoc/ui/TasksAdapter.java
@@ -16,11 +16,7 @@ import com.azubal.todoc.model.Task;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * <p>Adapter which handles the list of tasks to display in the dedicated RecyclerView.</p>
- *
- * @author Gaëtan HERFRAY
- */
+
 public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TaskViewHolder> {
     /**
      * The list of tasks the adapter deals with
